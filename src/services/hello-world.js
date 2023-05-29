88dafe404e4dc6bab9f54fe15f7534648fb19da4
Razvan Ioan Stoia <razvan.ioans96@gmail.com>
2023-05-29 20:36:37 +0200

    Prettier adjustments

--- src/services/hello-world.js
+++ src/services/hello-world.js
@@ -1,22 +1,18 @@
 class HelloWordService {
-    /**
-     * @description Create an instance of HelloWordService
-     */
-    constructor () {
-      
-    }
-  
-    /**
-     * @description Says Hello to a given name
-     * @param nameToHello {string} Name to greet
-     * greet name
-     * @returns a string that starts with Hello
-     */
-    greet ( nameToHello ) {
+  /**
+   * @description Create an instance of HelloWordService
+   */
+  constructor() {}
 
-        return "Hello " + nameToHello+"!";
-      
-    }
+  /**
+   * @description Says Hello to a given name
+   * @param nameToHello {string} Name to greet
+   * greet name
+   * @returns a string that starts with Hello
+   */
+  greet(nameToHello) {
+    return "Hello " + nameToHello + "!";
   }
-  
-  module.exports = HelloWordService;
+}
+
+module.exports = HelloWordService;
--- src/services/hello-world.test.js
+++ src/services/hello-world.test.js
@@ -1,10 +1,8 @@
 const HelloWordService = require("./hello-world");
-
 describe("HelloWordService Test", () => {
   const helloWordService = new HelloWordService();
 
   it("says 'Hello John!' to greet John", () => {
     expect(helloWordService.greet("John")).toBe("Hello John!");
   });
-
 });
